12b7bbe11913324942c64cff97659c67f5feda0c
dardan8 <dardan.dara@gmail.com>
2023-10-02 19:39:12 +0200

    quality

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,42 @@
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img
+          src={offer}
+          width={773}
+          height={687}
+          className='object-contain w-full'
+        />
+      </div>
+      <div className='flex flex-1 flex-col'>
+        <h2 className='mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+          Special
+          <span className='text-coral-red '> Offer </span>
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem{" "}
+        </p>
+        <p className='mt-6 lf:max-w-lg info-text'>
+          Molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+          nulla pariatur
+        </p>
+        <div className='flex flex-wrap gap-4'>
+          <Button label='Shop Now' iconURL={arrowRight} />
+          <Button
+            label='Learn More'
+            backgroundColor='bg-white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
